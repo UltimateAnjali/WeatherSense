3b7121cb87965ac8a890a615abd9ec9b89ce9282
UltimateAnjali <anjali21295@gmail.com>
2018-01-30 16:08:50 -0500

    color changes

--- app/src/main/java/com/katra/weathersense/activities/MainActivity.java
+++ app/src/main/java/com/katra/weathersense/activities/MainActivity.java
@@ -1,13 +1,20 @@
-package com.katra.weathersense;
+package com.katra.weathersense.activities;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.katra.weathersense.R;
+import com.katra.weathersense.classes.MyFonts;
 import com.katra.weathersense.dataModels.Channel;
 import com.katra.weathersense.services.WeatherServiceCallback;
 import com.katra.weathersense.services.YahooWeatherService;
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceCal
 
     private TextView tempText, locText, condText;
     private ImageView weatherIcon;
+    private MyFonts fontFace;
 
     private YahooWeatherService service;
 
@@ -24,10 +32,22 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceCal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
         tempText = (TextView)findViewById(R.id.tempTextView);
         locText = (TextView)findViewById(R.id.locTextView);
         condText = (TextView)findViewById(R.id.condTextView);
         weatherIcon = (ImageView)findViewById(R.id.weatherCondImageView);
+
+        fontFace = new MyFonts(getApplicationContext());
+        tempText.setTypeface(fontFace.getPoppins());
+        locText.setTypeface(fontFace.getSlabo());
+        condText.setTypeface(fontFace.getMerri());
 
         service = new YahooWeatherService(this);
         service.refreshWeather("Sydney, Australia");
---
+++ app/src/main/java/com/katra/weathersense/classes/MyFonts.java
@@ -0,0 +1,33 @@
+package com.katra.weathersense.classes;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by anjali desai on 30-01-2018.
+ */
+
+public class MyFonts {
+
+    Context mContext;
+    Typeface mTypeface;
+
+    public MyFonts(Context context){
+        this.mContext = context;
+    }
+
+    public Typeface getMerri(){
+        mTypeface = Typeface.createFromAsset(mContext.getAssets(),"fonts/Merriweather-Regular.ttf");
+        return mTypeface;
+    }
+
+    public Typeface getPoppins(){
+        mTypeface = Typeface.createFromAsset(mContext.getAssets(),"fonts/Poppins-Regular.ttf");
+        return mTypeface;
+    }
+
+    public Typeface getSlabo(){
+        mTypeface = Typeface.createFromAsset(mContext.getAssets(),"fonts/Slabo27px-Regular.ttf");
+        return mTypeface;
+    }
+}
